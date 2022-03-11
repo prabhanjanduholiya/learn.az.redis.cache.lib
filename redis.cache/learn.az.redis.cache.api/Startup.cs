455c97d8ced7eb94ddb9baf79920a90a4ecd6a08
Prabhanjan Duholiya <duholiya.prabhanjan@gmail.com>
2022-03-11 13:58:46 +0530

    Cleanup

--- redis.cache/learn.az.redis.cache.api/Startup.cs
+++ redis.cache/learn.az.redis.cache.api/Startup.cs
@@ -1,19 +1,10 @@
 using learn.az.redis.cache.lib;
-using learn.az.redis.cache.lib.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using StackExchange.Redis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace learn.az.redis.cache.api
 {
