d852dcbdcd26cc4ffd61f229aecea5be2f646e37
Prabhanjan Duholiya <prabhanjan.duholiya@nagarro.com>
2023-05-25 13:35:49 +0530

    Azure key vault demo project

---
+++ src/redis/key.vault/learn.az.key.vault.demo.api/Controllers/SecretsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace learn.az.key.vault.demo.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public SecretsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            List<string> result = new List<string>() {
+                _configuration["db-connection-string"]
+            };
+            return result;
+        }
+    }
+}
