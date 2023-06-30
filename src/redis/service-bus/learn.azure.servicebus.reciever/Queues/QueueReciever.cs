27b13ddc8ff03f870b52a822fcd475702c855bdc
Prabhanjan Duholiya <prabhanjan.duholiya@nagarro.com>
2023-06-30 10:29:57 +0530

    az service bus queue demo

---
+++ src/redis/service-bus/learn.azure.servicebus.reciever/Queues/QueueReciever.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn.azure.servicebus.reciever.Queues
+{
+    internal class QueueReciever
+    {
+        private readonly ServiceBusClient _client;
+
+        private readonly ServiceBusProcessor _processor;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="queueName"></param>
+        public QueueReciever(string connectionString, string queueName)
+        {
+            _client = new ServiceBusClient(connectionString);
+            _processor = _client.CreateProcessor(queueName);
+            _processor.ProcessMessageAsync += _processor_ProcessMessageAsync;
+            _processor.ProcessErrorAsync += _processor_ProcessErrorAsync;
+        }
+
+        private Task _processor_ProcessErrorAsync(ProcessErrorEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task _processor_ProcessMessageAsync(ProcessMessageEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/redis/service-bus/learn.azure.servicebus.sender/Queues/QueueSender.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn.azure.servicebus.sender.Queues
+{
+    internal class QueueSender : IDisposable
+    {
+        IQueueClient _queueClient;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <param name="queueName"></param>
+        public QueueSender(string connectionString, string queueName)
+        {
+            _queueClient = new QueueClient(connectionString, queueName);
+        }
+
+        public async void Dispose()
+        {
+            if (_queueClient != null && !_queueClient.IsClosedOrClosing)
+            {
+                await _queueClient.CloseAsync();
+            }
+        }
+
+        public async Task Send(string message)
+        {
+            Message servicebusMessage = new Message(Encoding.UTF8.GetBytes(message));
+
+            await _queueClient.SendAsync(servicebusMessage);
+        }
+    }
+}
