758b79c4f773fd226e9465897c6a5a96c0e61f41
Prabhanjan Duholiya <prabhanjan.duholiya@nagarro.com>
2023-07-05 16:40:40 +0530

    Config manager

--- src/redis/redis.cache/learn.az.redis.cache.lib/RedisConfigurationManager.cs
+++ src/redis/redis.cache/learn.az.redis.cache.lib/RedisConfigurationManager.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace learn.az.redis.cache.lib
 {
     /// <summary>
-    /// 
+    /// Config manager 
     /// </summary>
     public class RedisConfigurationManager
     {
